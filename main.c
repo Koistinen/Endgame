d75d22d6b5c78d8cd6de9dd55503f0384ebfb5ae
Koistinen <koistinen@teo.se>
2013-07-13 12:56:30 +0200

    Init with zeroes and save.

--- main.c
+++ main.c
@@ -2,17 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 typedef uint64_t Bitboard;
+typedef int8_t Byte;
 const char *PIECECHARS="K";
 
+void
+mark_legal(Byte *tb, int n, char *piece_types)
+{
+  int piece[33]; /* position of pieces */
+  int wtm;
+  int i;
+
+  for(wtm=2;wtm--;) {
+    piece[n]=1;
+    for(i=n;i--;) piece[i] = 077;
+    while(piece[n]) {
+      int pos = wtm<<(6*n);
+      for(i=n;i--;) {
+	pos += piece[i]<<(6*i);
+      }
+      {
+	tb[pos] = 0;
+      }
+      for(i=0;!piece[i]--;) {
+	piece[i++] = 077;
+      }
+    }
+  }
+}
+
+int
 main(int argc, char *argv[])
 {
-  int8_t *tb;
+  Byte *tb;
   int n;
   int i;
   int kings=0;
-  
+  int fd;
+  int sz;
+
   if (2 != argc) {
     printf("usage: %s KXKY\n", argv[0]);
     exit(1);
@@ -34,4 +67,18 @@ main(int argc, char *argv[])
     exit(1);
   }
   printf("Ending %s has %d men!\n", argv[1], n);
+  sz = 2<<(6*n);
+  tb = malloc(sz);
+  printf("Table has %d bytes.\n", sz);
+
+  /* compute */
+  mark_legal(tb, n, argv[1]);
+  
+  fd = creat(argv[1], 00666);
+  if (sz!=write(fd, tb, sz)) {
+    printf("Error: Failed to write the table properly.\n");
+    exit(1);
+  }
+
+  return 0;
 }
